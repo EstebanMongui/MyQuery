069465efbe3c3b49bfc79c746125fa66eb76ee92
Esteban Monguí <esteband.itorres@gmail.com>
2021-04-02 08:59:39 -0500

    Feat: integrate query with queries endpoint

--- App/src/components/QueryCard/QueryCard.jsx
+++ App/src/components/QueryCard/QueryCard.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./QueryCard.scss";
 import Tag from "../Tag/Tag";
 import AnswerCard from "../AnswerCard/AnswerCard"
 
 function QueryCard(props){
+    
+    // eslint-disable-next-line no-unused-vars
+    const [query, setQuery] = useState([]);
+    const URL = 'http://127.0.0.1:8000/queries'
+
+
+    useEffect(()=>{
+        fetch(URL)
+        .then((response)=>{
+            return response.json()
+        })
+        .then((queries)=>{
+            console.log(queries)
+            setQuery(queries)
+        })
+    })
+
     return(
         <React.Fragment>
             <div className="QueryCard">
