306158e19e4e864d407dbd1df42a1f3ce27887a4
Esteban Monguí <esteband.itorres@gmail.com>
2021-04-03 11:50:04 -0500

    feat: create is answered field in query model

--- Api/my_query/urls.py
+++ Api/my_query/urls.py
@@ -6,6 +6,7 @@ from django.contrib import admin
 from rest_framework.urlpatterns import format_suffix_patterns
 # Views
 from queries import views
+from users import views as UsersViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,6 +14,7 @@ urlpatterns = [
     path('queries/', views.QueryList.as_view()),
     path('queries/<str:category>/', views.QueryFilter.as_view()),
     path('answers/', views.AnswerList.as_view()),
+    path('users/', UsersViews.LocalUserList.as_view()),
     # path('access/<int:age>', views.access),
 ]
 
